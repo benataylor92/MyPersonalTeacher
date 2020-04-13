df831d53bbe8b98286be300f53a367162d09a98e
Ben Taylor <TAYLORBX@legal.regn.net>
2020-04-13 14:40:05 +0100

    [BT/RS] added linkd between the header buttons and pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 import '../Scss/_Header.css'
 
 export default class Header extends React.Component{
   render() {
     return (
     <div className="Header-Bar">
-    <button className="home-logo">Vintage 4 You</button>
-    <button className="Men-Button">Men</button>
-    <button className="Women-Button">Women</button>
+    <Link to="/" className="home-logo">Vintage 4 You</Link>
+    <Link to="/menPage" className="Men-Button">Men</Link>
+    <Link to="/womenPage" className="Women-Button">Women</Link>
   </div>
     )
   }
