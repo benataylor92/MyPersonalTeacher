d5aff411ebaa9efb50bbd3289d40bbf18e25510b
Ben Taylor <TAYLORBX@legal.regn.net>
2020-04-24 14:18:28 +0100

    added states to firstname on sign up page

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import '../Scss/_SignUp.css';
+import { render } from '@testing-library/react';
 
-export default function SignUp() {
+export default class SignUp extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+        user: {
+            firstName: '',
+            lastName: '',
+            emailadress: '',
+            confirmemailaddress: ''
+        },
+        // submitted: false;
+    };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    render() {
+
+    const { user } = this.state;
+
     return (
         <div className="Sign-up-box">
             <div className="Register">Register</div>
             <form className="Login-form">
                 <div className="Form-group">
                     <label htmlfor="First-name">First Name</label>
-                    <input type="text" className="Form-control" name="firstname" value />
+                    <input type="text" className="Form-control" name="firstname" value={user.firstname} onChange={this.handleChange}/>
                 <div>
                     <label htmlfor="Last-name">Last Name</label>
-                    <input type="text" className="Form-control" name="lastname" value />
+                    <input type="text" className="Form-control" name="lastname" value={user.lastName} />
                 </div>
                 <div>
                     <label htmlfor="Email-adress">Email Adress</label>
-                    <input type="text" className="Form-control" name="emailaddress" value />
+                    <input type="text" className="Form-control" name="emailaddress" value={user.emailadress} />
                 </div>
                 <div>
                     <label htmlfor="Confirm-email-adress">Confirm Email Adress</label>
-                    <input type="text" className="Form-control" name="confirmemailaddress" value />
+                    <input type="text" className="Form-control" name="confirmemailaddress" value={user.confirmemailaddress} />
                 </div>
                 </div>
             </form>
         </div>
     ); 
+    };
 }
