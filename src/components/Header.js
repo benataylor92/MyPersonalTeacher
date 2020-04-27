52310aadb299aea629180e8a562e0b9d5e4a8d93
Ben Taylor <TAYLORBX@legal.regn.net>
2020-04-27 17:44:21 +0100

    added chevron directions for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import '../Scss/_Header.css'
 import Logo from './MSB.png';
+import '@progress/kendo-theme-default/dist/all.css';
 
 export default class Header extends React.Component{
   render() {
@@ -15,6 +16,7 @@ export default class Header extends React.Component{
     <img src={Logo} alt="Website-Logo" className="Website-Logo" onClick={() => logoImageClick()} />
     <div className="Header-links">
       <Link to="/KS1" className="KS1-Button">Key Stage 1</Link>
+      <span class="k-icon my-custom-icon-class"></span>
       <Link to="/KS2" className="KS2-Button">Key Stage 2</Link>
       <Link to="/KS3" className="KS3-Button">Key Stage 3</Link>
       <Link to="/KS4" className="KS4-Button">Key Stage 4</Link>
