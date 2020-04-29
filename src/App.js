47f85baab103a1e51249d8712f0aa61acdb77489
Ben Taylor <TAYLORBX@legal.regn.net>
2020-04-29 17:59:46 +0100

    added chevronto the header bar and the npm packages with it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Routes from '../src/routes/index';
-import '@progress/kendo-theme-default/dist/all.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import '../Scss/_Header.css'
 import Logo from './MSB.png';
-import '@progress/kendo-theme-default/dist/all.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 export default class Header extends React.Component{
   render() {
@@ -16,11 +17,15 @@ export default class Header extends React.Component{
     <img src={Logo} alt="Website-Logo" className="Website-Logo" onClick={() => logoImageClick()} />
     <div className="Header-links">
       <Link to="/KS1" className="KS1-Button">Key Stage 1</Link>
-      <span class="k-icon my-custom-icon-class"></span>
+      <FontAwesomeIcon className="chevron-up" icon={faChevronUp} />
       <Link to="/KS2" className="KS2-Button">Key Stage 2</Link>
+      <FontAwesomeIcon className="chevron-up" icon={faChevronUp} />
       <Link to="/KS3" className="KS3-Button">Key Stage 3</Link>
+      <FontAwesomeIcon className="chevron-up" icon={faChevronUp} />
       <Link to="/KS4" className="KS4-Button">Key Stage 4</Link>
+      <FontAwesomeIcon className="chevron-up" icon={faChevronUp} />
       <Link to="/KS5" className="KS5-Button">Key Stage 5</Link>
+      <FontAwesomeIcon className="chevron-up" icon={faChevronUp} />
       <Link to="/loginPage" className="Login-Button">Login</Link> 
       <Link to="/signup" className="Sign-up">Sign Up</Link>
       </div>
