20f1ceb9d1637afbbcb4c7a7bd6c8efcde2da745
Ben Taylor <TAYLORBX@legal.regn.net>
2020-05-05 14:55:05 +0100

    added new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../Scss/_Header.css'
-import Logo from './MSB.png';
+import Logo from './Logo.png';
 import SearchBar from './SearchBar';
 import HeaderBanner from './HeaderBanner';
 
@@ -15,7 +15,6 @@ export default class Header extends React.Component{
 
     return (
       <div>
-    {/* // <div className="Header-Bar"> */}
       <img src={Logo} alt="Website-Logo" className="Website-Logo" onClick={() => logoImageClick()} />
       <SearchBar />
       <HeaderBanner />
