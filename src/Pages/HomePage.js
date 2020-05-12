bbc8b68de5e0153e85d69478b52d5749190b6a48
Ben Taylor <TAYLORBX@legal.regn.net>
2020-05-12 13:23:13 +0100

    added kS buttons to home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -66,12 +66,19 @@ export default function HomePage() {
                     </div>         
                 </div>
                 <div className="QuickClickMaterial">
-                    <div className="QuickClickMaterial-Box">Test
+                    <div className="QuickClickMaterial-Box">
                         <div className="QuickClickMaterial-Box-SideBar">The UK's largest online teaching resource
                             <div className="QuickClickMaterial-Box-SideBar-Text">Choose from over 30 minutes of online courses with new additions published every week
                             </div>
                         </div>
-                        <div className="QuickClickMaterial-Box-TopFiler"> Top Filter
+                        <div className="QuickClickMaterial-Box-TopFiler">
+                          <div className="KSButtons">
+                          <button className="KS1Button">Key-Stage 1</button>
+                          <button className="KS2Button">Key-Stage 3</button>
+                          <button className="KS3Button">Key-Stage 4</button>
+                          <button className="KS4Button">Key-Stage 2</button>
+                          <button className="KS5Button">Key-Stage 5</button>
+                          </div>
                         </div>
                     </div>
                 </div>
